844c122075e4cb6f4a945850ce7fd8397c6d207e
Yogaraj <yogarajjuju22@gmail.com>
2025-09-09 18:28:50 +0530

    first commit

---
+++ src/Example.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class Example {
+    public static void main(String[] args) {
+        Stack<Integer> stack =new Stack<>();
+        stack.push(12);
+        stack.push(66);
+        stack.push(13);
+        stack.push(80);
+        stack.push(22);
+        stack.push(21);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+
+
+    }
+}
