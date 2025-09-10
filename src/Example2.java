4aad6790de8e7f5c3c7ba82c5b8d4183225701a8
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-10 09:59:43 +0530

    banaki

---
+++ src/Example2.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Example2 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(32);
+        queue.add(41);
+        queue.add(2);
+        queue.add(3);
+        queue.add(10);
+        queue.add(12);
+        System.out.println(queue);
+        System.out.println(queue.remove());
+        System.out.println(queue);
+
+    }
+}
