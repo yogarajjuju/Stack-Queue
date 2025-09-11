437b3f18c672200f91f36f99c95baa69fb495e11
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-11 09:53:36 +0530

    banaki

--- src/CustomQueue.java
+++ src/CustomQueue.java
@@ -1,21 +1,60 @@
 public class CustomQueue {
 
     private int data[];
-    private  static final int DEFAULT_SIZE=10;
+    private static final int DEFAULT_SIZE = 10;
 
-    int end=-1;
+    int end = -1;
 
     public CustomQueue() {
         this(DEFAULT_SIZE);
     }
-    public CustomQueue(int size){
-        this.data= new int[size];
+
+    public CustomQueue(int size) {
+        this.data = new int[size];
     }
 
-    public boolean isFull(){
-        return end== data.length-1;
+    public boolean isFull() {
+        return end == data.length - 1;
     }
-    public boolean isEmpty(){
-        return end ==-1;
+
+    public boolean isEmpty() {
+        return end == -1;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[++end] = item; // increment first, then insert
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("The Queue is empty ");
+        }
+
+        int removed = data[0];
+        // shift elements to the left
+        for (int i = 1; i <= end; i++) {
+            data[i - 1] = data[i];
+        }
+        end--;
+
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("The Queue is empty");
+        }
+        return data[0];
+    }
+
+    public void display() {
+        for (int i = 0; i <= end; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("END");
     }
 }
---
+++ src/QueueMain.java
@@ -0,0 +1,16 @@
+public class QueueMain {
+    public static void main(String[] args) {
+        CustomQueue queue = new CustomQueue(5);
+
+            queue.insert(20);
+        queue.insert(50);
+        queue.insert(26);
+        queue.insert(57);
+        queue.insert(17);
+        queue.insert(14);
+        queue.display();
+
+
+
+    }
+}
