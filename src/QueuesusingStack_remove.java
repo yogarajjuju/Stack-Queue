e9e610fe5ca474e3302987afd24434f019f4f496
yogarajjuj <yogarajjuju22@gmail.com>
2025-09-15 11:41:41 +0530

    banaki

---
+++ src/QueuesusingStack_remove.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class QueuesusingStack_remove {
+   private  Stack<Integer> first;
+  private   Stack<Integer> second;
+
+  public QueuesusingStack_remove(){
+      first= new Stack<>();
+      second= new Stack<>();
+
+  }
+
+  public void add(int item){
+      while(!first.isEmpty()){
+          second.push(first.pop());
+      }
+      first.push(item);
+      while(!second.isEmpty()){
+          first.push(second.pop())
+      }
+  }
+
+
+  public int remove(){
+      return first.pop();
+  }
+
+  public boolean isEmpty(){
+      return first.isEmpty();
+  }
+
+
+}
